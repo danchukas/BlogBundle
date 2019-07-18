9cf15ee3f41faa6f1dde7ff37821f6964233ff0b
danchukas <volk160590@mail.ru>
2019-07-18 14:18:45 +0300

    Update AbstractController.php
    bit cleared code

--- Controller/AbstractController.php
+++ Controller/AbstractController.php
@@ -5,26 +5,19 @@ namespace Stfalcon\Bundle\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
- * TagController
- *
  * @author Stepan Tanasiychuk <ceo@stfalcon.com>
  */
 class AbstractController extends Controller
 {
-
-    /**
-     * Added disqus_shortname to request data array
-     *
-     * @param array $requestData
-     * @return array
-     */
-    protected function _getRequestDataWithDisqusShortname($requestData)
+    protected function _getRequestDataWithDisqusShortname(array $requestData): array
     {
         $config = $this->container->getParameter('stfalcon_blog.config');
-        return array_merge(
+        $disqusShortnameKey = 'disqus_shortname';
+        $requestDataWithDisqusShortname = array_merge(
             $requestData,
-            array('disqus_shortname' => $config['disqus_shortname'])
+            [$disqusShortnameKey => $config[$disqusShortnameKey]]
         );
+        
+        return $requestDataWithDisqusShortname;
     }
-
 }
